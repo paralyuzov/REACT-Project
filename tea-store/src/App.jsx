95aa3b13b0d06b3cd0afde60dd804ab2e21f2a46
paralyuzov <mparalyuzov@gmail.com>
2024-08-01 23:06:07 +0300

    add PrepSencha component

--- tea-store/src/App.jsx
+++ tea-store/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/header/Header'
 import Main from './components/home/Main'
 import AboutTheTea from './components/about/about-the-tea/AboutTheTea';
 import Preparing from './components/about/preparing-tea/Preparing';
+import PrepSencha from './components/about/preparing-tea/sencha/PrepSencha';
 
 
 
@@ -24,6 +25,7 @@ function App() {
                             <Route path='/' element={<Main />} />
                             <Route path='/about-the-tea' element={<AboutTheTea />} />
                             <Route path='/preparing-tea' element={<Preparing />} />
+                            <Route path='/preparing-tea/sencha' element={<PrepSencha />} />
 
                         </Routes>
                     </div>
---
+++ tea-store/src/components/about/preparing-tea/sencha/PrepSencha.jsx
@@ -0,0 +1,52 @@
+
+export default function PrepSencha() {
+
+
+    return (
+        <div className="flex flex-col justify-center gap-10 items-center">
+            <div className="flex flex-col gap-5 pb-10 border-b-2 mt-10 font-kreon w-full">
+                <h3 className="text-3xl text-yellow-600">Verdant fragrance and energetic flavour</h3>
+                <p className="text-xl">Its tender characteristics come from the freshly picked tea buds.<br />
+                    Brew for slightly less time than usual.</p>
+                <p className="text-xl">Tip: Shincha 2024, brew for slightly longer, for 50 seconds. A 40-second brew (the recipe printed on the Shincha package) will produce a lighter taste.</p>
+            </div>
+            <div className="mt-10">
+                <div className="pb-20">
+                    <h3 className="text-5xl">Basic Shincha</h3>
+                </div>
+                <div className="flex justify-center items-center max-w-2xl text-2xl border-2 rounded-xl p-8 font-laila">
+                    <p className="border-r-2 border-black px-5 flex justify-center items-center gap-2"><i className="fa-brands fa-envira"></i>10g</p>
+                    <p className="border-r-2 border-black px-5 flex justify-center items-center gap-2"><i className="fa-solid fa-mug-saucer"></i>210ml</p>
+                    <p className="border-r-2 border-black px-5 flex justify-center items-center gap-2"><i className="fa-solid fa-temperature-three-quarters"></i>80℃</p>
+                    <p className="px-5 flex justify-center items-center gap-2"><i className="fa-regular fa-clock"></i>40 sec.</p>
+                </div>
+            </div>
+
+            <div className="grid grid-rows3 gap-10">
+
+                <div className="flex items-center gap-5">
+                    <div className="max-w-[200px]">
+                        <img src="\src\assets\prep\sencha\sencha1.png" alt="" />
+                    </div>
+                    <p className="text-xl leading-loose text-start">Use 10g of leaves (2 tablespoons)</p>
+                </div>
+
+                <div className="flex items-center gap-5 ">
+                    <div className="max-w-[200px]">
+                        <img src="\src\assets\prep\sencha\sencha2.png" alt="" />
+                    </div>
+                    <p className="text-xl leading-loose text-start">Add 210ml of hot water (80°C)</p>
+                </div>
+
+                <div className="flex items-center gap-5">
+                    <div className="max-w-[200px]" >
+                        <img src="\src\assets\prep\sencha\sencha3.png" alt="" />
+                    </div>
+                    <p className="text-xl leading-loose text-start">Serve 40 seconds after beginning to pour the hot water.<br />
+                    *Shincha 2024, brew for slightly longer, for 50 seconds.</p>
+                </div>
+
+            </div>
+        </div>
+    );
+}
