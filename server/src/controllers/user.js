80dfb753a4d5df88b85e5e78c6ea08c3542007fc
paralyuzov <mparalyuzov@gmail.com>
2024-08-03 02:35:00 +0300

    add backend functionality to update user profile

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { body, validationResult } = require('express-validator');
 const { createToken } = require('../services/jwt');
-const { login, register } = require('../services/user');
+const { login, register, update } = require('../services/user');
 const { parseError } = require('../utils/parseError');
+const { User } = require('../models/User');
 
 
 const userRouter = Router();
@@ -59,5 +60,46 @@ userRouter.post('/register',
             res.status(403).json({ code: 403, message: parsed.message })
         }
     })
+
+userRouter.put('/update/:id', body('email').trim().isEmail().withMessage('Please enter valid email'),
+    body('username').optional().trim().notEmpty().withMessage('Username cannot be empty'),
+    async (req, res) => {
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors
+            }
+            console.log(req.body.username);
+            const result = await update(req.params.id, req.body.username, req.body.email, req.body.tel);
+            const accessToken = createToken(result);
+            res.status(201).json({
+                _id: result._id,
+                username: result.username,
+                email: result.email,
+                tel: result.tel,
+                accessToken
+
+            })
+
+
+        } catch (error) {
+            const parsed = parseError(error);
+            res.status(403).json({ code: 403, message: parsed.message });
+        }
+    })
+
+userRouter.delete('/delete/:id', async (req, res) => {
+    try {
+
+        const userId = req.params.id.toString();
+        await User.findByIdAndDelete(userId);
+
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        const parsed = parseError(error);
+        res.status(403).json({ code: 403, message: parsed.message });
+    }
+});
 
 module.exports = { userRouter };
