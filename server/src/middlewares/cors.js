926bbe3df1ffc08f5552acffc924ae1a72fedfbe
paralyuzov <mparalyuzov@gmail.com>
2024-08-03 02:34:21 +0300

    update cors middleware

--- server/src/middlewares/cors.js
+++ server/src/middlewares/cors.js
@@ -1,10 +1,16 @@
 function cors() {
-    return function(req,res,next) {
-        res.setHeader('Access-Control-Allow-Origin','*');
-        res.setHeader('Access-Control-Allow-Method','OPTIONS,GET,POST,PUT,DELETE');
-        res.setHeader('Access-Control-Allow-Headers','Content-Type,X-Authorization');
-        next()
-    }
+    return function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+
+        
+        if (req.method === 'OPTIONS') {
+            res.sendStatus(204);
+        } else {
+            next();
+        }
+    };
 }
 
-module.exports = {cors}
+module.exports = { cors }
