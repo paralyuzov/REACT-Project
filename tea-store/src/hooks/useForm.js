d33e977a99a2605a237318f06d580bc313b8daeb
paralyuzov <mparalyuzov@gmail.com>
2024-08-11 13:49:06 +0300

    Update useForm.js
    add validation logic

--- tea-store/src/hooks/useForm.js
+++ tea-store/src/hooks/useForm.js
@@ -1,16 +1,34 @@
 import { useState } from 'react';
 
-export function useForm(initialValues, submitCallback) {
+export function useForm(initialValues, submitCallback, validate) {
     const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
 
     const changeHandler = (e) => {
         setValues(prevState => ({
             ...prevState, [e.target.name]: e.target.value
         }));
+
+        setErrors(prevErrors => {
+            const validationErrors = validate({ ...values, [e.target.name]: e.target.value });
+            if (!validationErrors[e.target.name]) {
+                const { [e.target.name]: removedError, ...rest } = prevErrors;
+                return rest;
+            }
+            return {
+                ...prevErrors,
+                [e.target.name]: validationErrors[e.target.name],
+            };
+        });
     }
 
     const submitHandler = (e) => {
         e.preventDefault();
+        const validationErrors = validate(values);
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
 
         submitCallback(values);
     }
@@ -22,5 +40,5 @@ export function useForm(initialValues, submitCallback) {
         }));
     };
 
-    return { values, changeHandler, submitHandler,updateValues };
+    return { values, changeHandler, submitHandler, updateValues, errors };
 }
