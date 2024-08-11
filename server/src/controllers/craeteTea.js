2a664d251a16e8b8db7684c48339b7bde2e6651e
paralyuzov <mparalyuzov@gmail.com>
2024-08-11 17:13:12 +0300

    Update craeteTea.js
    add response status code

--- server/src/controllers/craeteTea.js
+++ server/src/controllers/craeteTea.js
@@ -1,21 +1,22 @@
 const { Router } = require('express');
 
 const { create } = require("../services/teas");
+const { parseError } = require('../utils/parseError');
 
 const createTea = Router();
 
 
 createTea.post('/tea', async (req, res) => {
-    const {title,type,price,package,weight,serving,ingredients,image,life,description} = req.body
+    const { title, type, price, package, weight, serving, ingredients, image, life, description } = req.body
 
 
     try {
-        await create({title,type,price,package,weight,serving,ingredients,image,life,description});
-        res.send('Succsefull')
+        await create({ title, type, price, package, weight, serving, ingredients, image, life, description });
+        res.status(200).json("success")
 
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ message: parseError(error) });
     }
 })
 
-module.exports = {createTea}
+module.exports = { createTea }
