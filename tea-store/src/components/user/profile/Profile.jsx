d8ea3ab6afb503e836f8429846407754ba79aa0d
paralyuzov <mparalyuzov@gmail.com>
2024-08-13 02:06:58 +0300

    Update Profile.jsx

--- tea-store/src/components/user/profile/Profile.jsx
+++ tea-store/src/components/user/profile/Profile.jsx
@@ -5,26 +5,7 @@ import { useForm } from "../../../hooks/useForm";
 import requester from "../../../api/requester";
 import ProfileModal from "../../modals/ProfileModal";
 import ConfirmModal from "../../modals/ConfirmModal";
-
-const validateProfile = (values) => {
-    const errors = {};
-    if (!values.username) {
-        errors.username = 'Username is required';
-    } else if (values.username.length < 4) {
-        errors.username = "Username must be at least 4 characters";
-    }
-    if (!values.email) {
-        errors.email = 'Email is required';
-    } else if (values.email.length < 10) {
-        errors.email = "Email must be at least 10 characters";
-    } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(values.email)) {
-        errors.email = 'Email address is invalid';
-    }
-    if (!values.tel) {
-        errors.tel = 'Telephone is required';
-    }
-    return errors;
-};
+import { validateProfile } from "./validateProfile";
 
 
 export default function Profile() {
