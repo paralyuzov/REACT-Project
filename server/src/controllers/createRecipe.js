09fba47091e419246129643bfd332da5bcf85756
paralyuzov <mparalyuzov@gmail.com>
2024-08-12 16:37:35 +0300

    Update createRecipe.js

--- server/src/controllers/createRecipe.js
+++ server/src/controllers/createRecipe.js
@@ -5,13 +5,25 @@ const { createRecipeModel } = require("../services/create");
 const createRecipe = Router();
 
 
-createRecipe.post('/recipe', async (req, res) => {
-    const { title, info, image, preparing } = req.body
+createRecipe.post('/recipes', async (req, res) => {
+    const data = req.body;
 
+
+    const createData = {
+        title: data.title,
+        info: data.info,
+        image: data.image,
+        preparing: {
+            quantity: data.quantity,
+            amount: data.amount,
+            time: data.time,
+            aditional: data.aditional
+        }
+    }
 
     try {
-        await createRecipeModel({ title, info, image, preparing });
-        res.send('Succsefull')
+        await createRecipeModel(createData);
+        res.status(200).json('Succsefull')
 
     } catch (error) {
         console.log(error)
