c98b45f76f8bd12cd5728f41ef325ba8541cfcb1
paralyuzov <mparalyuzov@gmail.com>
2024-08-13 02:02:57 +0300

    Update CreateUtensil.jsx

--- tea-store/src/components/user/admin/utensils/CreateUtensil.jsx
+++ tea-store/src/components/user/admin/utensils/CreateUtensil.jsx
@@ -1,7 +1,9 @@
 import { useForm } from "../../../../hooks/useForm";
 import { post } from "../../../../api/requester";
 import { useState } from "react";
-
+import { validateUtensil } from "./validateUtensils";
+import UponRequest from "../../../modals/UponRequest";
+import ErrorFormModal from "../../../modals/ErrorFormModal";
 
 
 const initialValues = {
@@ -12,65 +14,43 @@ const initialValues = {
     description: '',
 };
 
-const validate = (values) => {
-    const errors = {};
-
-    if (!values.title) errors.title = 'Title is required';
-    if (!values.price) errors.price = 'Price is required';
-    if (!values.package) errors.package = 'Package dimension is required';
-    if (!values.image) errors.image = 'Image URL is required';
-    if (!values.description) errors.description = 'Description is required';
-
-    return errors;
-};
 
 export default function CreateUtensil() {
 
-
-
-    const [modal, setModal] = useState({ visible: false, content: '', type: '' });
-    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errModal, setErrModal] = useState(false);
+    const [reqModal, setReqModal] = useState(false);
+    const [message, setMessage] = useState("");
 
     const submitCallback = async (formValues) => {
         try {
-            setIsSubmitting(true);
             await post('http://localhost:3030/data/utensils', formValues);
-            setModal({
-                visible: true,
-                content: 'Utensil added successfully!',
-                type: 'success',
-            });
-            updateValues(initialValues); 
+            setMessage('Utensil added successfully!');
+            setReqModal(true)
+            updateValues(initialValues);
         } catch (err) {
-            setModal({
-                visible: true,
-                content: 'Error adding utensil. Please try again.',
-                type: 'error',
-            });
-        } finally {
-            setIsSubmitting(false);
+            setMessage('Failed to add the utensil.');
+            setReqModal(true)
         }
     };
 
-    const { values, changeHandler, submitHandler, updateValues, errors } = useForm(initialValues, submitCallback, validate);
+    const { values, changeHandler, submitHandler, updateValues, errors } = useForm(initialValues, submitCallback, validateUtensil);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        const formErrors = validate(values);
-        if (Object.keys(formErrors).length > 0) {
-            setModal({
-                visible: true,
-                content: Object.values(formErrors).join(', '),
-                type: 'error',
-            });
+        if (Object.keys(errors).length > 0) {
+            setErrModal(true);
         } else {
             submitHandler(e);
         }
     };
 
-    const closeModal = () => {
-        setModal({ ...modal, visible: false });
+    const closeReqModal = () => {
+        setReqModal(false);
+    }
+
+
+    const closeErrModal = () => {
+        setErrModal(false);
     };
 
     return (
@@ -88,24 +68,23 @@ export default function CreateUtensil() {
                                     type="text"
                                     value={values.title}
                                     onChange={changeHandler}
-                                    className={`bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all ${errors.title ? 'border-red-500' : ''}`}
+                                    className="bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all"
                                     placeholder="Enter tea name"
                                 />
-                                {errors.title && <p className="text-red-500 text-sm">{errors.title}</p>}
                             </div>
-                            
+
                             <div>
                                 <label htmlFor='price' className="text-gray-800 text-xl mb-2 block">Price</label>
                                 <input
                                     name="price"
                                     id='price'
-                                    type="text"
+                                    type="number"
                                     value={values.price}
                                     onChange={changeHandler}
-                                    className={`bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all ${errors.price ? 'border-red-500' : ''}`}
+                                    className="bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all"
                                     placeholder="Enter price"
                                 />
-                                {errors.price && <p className="text-red-500 text-sm">{errors.price}</p>}
+
                             </div>
                             <div>
                                 <label htmlFor='package' className="text-gray-800 text-xl mb-2 block">Box size</label>
@@ -115,12 +94,12 @@ export default function CreateUtensil() {
                                     type="text"
                                     value={values.package}
                                     onChange={changeHandler}
-                                    className={`bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all ${errors.package ? 'border-red-500' : ''}`}
+                                    className="bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all"
                                     placeholder="example : W8.6 x D8.2 x H14.1 cm"
                                 />
-                                {errors.package && <p className="text-red-500 text-sm">{errors.package}</p>}
+
                             </div>
-                            
+
                             <div>
                                 <label htmlFor='image' className="text-gray-800 text-xl mb-2 block">Image URL</label>
                                 <input
@@ -129,10 +108,10 @@ export default function CreateUtensil() {
                                     type="text"
                                     value={values.image}
                                     onChange={changeHandler}
-                                    className={`bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all ${errors.image ? 'border-red-500' : ''}`}
+                                    className="bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all"
                                     placeholder="Enter image URL https://...."
                                 />
-                                {errors.image && <p className="text-red-500 text-sm">{errors.image}</p>}
+
                             </div>
 
                             <div>
@@ -142,19 +121,18 @@ export default function CreateUtensil() {
                                     id='description'
                                     value={values.description}
                                     onChange={changeHandler}
-                                    className={`bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all ${errors.description ? 'border-red-500' : ''}`}
+                                    className="bg-gray-100 w-full text-gray-800 text-xl px-4 py-4 focus:bg-transparent outline-lime-200 transition-all "
                                     placeholder="Enter description"
                                 />
-                                {errors.description && <p className="text-red-500 text-sm">{errors.description}</p>}
+
                             </div>
                         </div>
 
                         <div className="mt-12">
                             <input
                                 type="submit"
-                                value={isSubmitting ? "Adding..." : "Add Utensil"}
+                                value="Add Utensil"
                                 className="py-4 px-8 text-xl font-semibold text-black tracking-wide bg-lime-200 hover:bg-lime-300 focus:outline-none"
-                                disabled={isSubmitting}
                             />
                         </div>
                     </form>
@@ -168,21 +146,8 @@ export default function CreateUtensil() {
                     </div>
                 </div>
             </div>
-
-            {modal.visible && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-white p-6 rounded shadow-md">
-                        <h2 className="text-xl font-bold mb-4">{modal.type === 'success' ? 'Success' : 'Error'}</h2>
-                        <p className="text-gray-700 mb-4">{modal.content}</p>
-                        <button
-                            onClick={closeModal}
-                            className="px-4 py-2 bg-red-500 text-white rounded"
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            )}
+            {reqModal && <UponRequest message={message} onClose={closeReqModal} />}
+            {errModal && <ErrorFormModal errors={errors} onClose={closeErrModal} />}
         </div>
     );
 }
