02812ef4c5696b3feb7c8df1a9a55ab6654d2875
paralyuzov <mparalyuzov@gmail.com>
2024-08-13 16:17:09 +0300

    Update ControlPanel.jsx

--- tea-store/src/components/user/admin/ControlPanel.jsx
+++ tea-store/src/components/user/admin/ControlPanel.jsx
@@ -9,7 +9,7 @@ export default function ControlPanel() {
             </div>
             <div className="grid grid-cols-4 items-center gap-5  p-5 text-2xl font-kreon">
                 <Link to={'/admin/teas'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105 hover:bg-slate-50">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105">
                         <div>
                             <img className="w-40 h-40" src="\src\assets\admin-panel\add-tea.jpg" alt="" />
                         </div>
@@ -18,7 +18,7 @@ export default function ControlPanel() {
                 </Link>
 
                 <Link to={'/admin/utensils'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105 hover:bg-slate-50 ">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105">
                         <div >
                             <img className="w-40 h-40" src="\src\assets\admin-panel\add-utensils.jpg" alt="" />
                         </div>
@@ -27,7 +27,7 @@ export default function ControlPanel() {
                 </Link>
 
                 <Link to={'/admin/recipes'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105  hover:bg-slate-50">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105">
                         <div>
                             <img className="w-40 h-40" src="\src\assets\admin-panel\add-recipe.avif" alt="" />
                         </div>
@@ -36,7 +36,7 @@ export default function ControlPanel() {
                 </Link>
 
                 <Link to={'/admin/users'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105  hover:border-black">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105">
                         <div>
                             <img className="w-40 h-40" src="\src\assets\admin-panel\users.webp" alt="" />
                         </div>
