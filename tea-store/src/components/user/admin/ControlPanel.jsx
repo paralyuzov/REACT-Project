465ba9bc94958ed5892a74093edec990eeb9ac97
paralyuzov <mparalyuzov@gmail.com>
2024-08-13 16:08:34 +0300

    Update ControlPanel.jsx

--- tea-store/src/components/user/admin/ControlPanel.jsx
+++ tea-store/src/components/user/admin/ControlPanel.jsx
@@ -7,38 +7,38 @@ export default function ControlPanel() {
             <div>
                 <h2 className="text-5xl my-10">Control Panel</h2>
             </div>
-            <div className="grid grid-cols-3 items-center gap-5  p-5 text-2xl font-kreon">
+            <div className="grid grid-cols-4 items-center gap-5  p-5 text-2xl font-kreon">
                 <Link to={'/admin/teas'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-full hover:scale-105 hover:bg-slate-50">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105 hover:bg-slate-50">
                         <div>
-                            <img className="w-44 h-44" src="\src\assets\admin-panel\add-tea.jpg" alt="" />
+                            <img className="w-40 h-40" src="\src\assets\admin-panel\add-tea.jpg" alt="" />
                         </div>
                         <p className="py-2">Teas</p>
                     </div>
                 </Link>
 
                 <Link to={'/admin/utensils'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-full hover:scale-105 hover:bg-slate-50 ">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105 hover:bg-slate-50 ">
                         <div >
-                            <img className="w-44 h-44" src="\src\assets\admin-panel\add-utensils.jpg" alt="" />
+                            <img className="w-40 h-40" src="\src\assets\admin-panel\add-utensils.jpg" alt="" />
                         </div>
                         <p className="py-2">Utensils</p>
                     </div>
                 </Link>
 
                 <Link to={'/admin/recipes'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-full hover:scale-105  hover:bg-slate-50">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105  hover:bg-slate-50">
                         <div>
-                            <img className="w-44 h-44" src="\src\assets\admin-panel\add-recipe.avif" alt="" />
+                            <img className="w-40 h-40" src="\src\assets\admin-panel\add-recipe.avif" alt="" />
                         </div>
                         <p className="py-2">Recipes</p>
                     </div>
                 </Link>
 
                 <Link to={'/admin/users'}>
-                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-full hover:scale-105  hover:bg-slate-50">
+                    <div className="flex flex-col justify-center items-center gap-5 border-2 rounded-3xl hover:scale-105  hover:border-black">
                         <div>
-                            <img className="w-44 h-44" src="\src\assets\admin-panel\users.webp" alt="" />
+                            <img className="w-40 h-40" src="\src\assets\admin-panel\users.webp" alt="" />
                         </div>
                         <p className="py-2">Users</p>
                     </div>
